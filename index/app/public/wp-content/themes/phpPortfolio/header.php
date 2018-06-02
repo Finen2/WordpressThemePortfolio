76f3107a08962ddeab0613e457c4068fb1eaa66b
Tim Aro <timaro@live.se>
2018-06-02 18:42:54 +0200

    not loadning css yet && starting structure with navbar

--- index/app/public/wp-content/themes/phpPortfolio/header.php
+++ index/app/public/wp-content/themes/phpPortfolio/header.php
@@ -1,5 +1,15 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Tutorial theme</title>
+    <script type="text/javascript" src="<?php echo get_stylesheet_directory_uri().'/js/bootstrap.js'; ?>"></script>
+    <link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri().'/css/bootstrap.css'; ?>">
+    <link rel="stylesheet" type="text/css" media="all" href="<?php bloginfo( 'stylesheet_url' ); ?>" />
+  </head>
 
-  <div id="ttr_header" class="jumbotron">
-    <h1>HEADER</h1>
-  </div>
-  
+  <body>
+
+    <div id="ttr_header" class="jumbotron">
+      <?php include('navbar.php'); ?>
+    </div>
+    <div class="container">
---
+++ index/app/public/wp-content/themes/phpPortfolio/post.php
@@ -0,0 +1,5 @@
+<div class="col-lg-12 col-sm-12 col-md-12 col-xs-12 backgroundColor">
+  <h1><?php the_title(); ?></h1>
+  <h4>Posted on <?php the_time('F jS, Y') ?></h4>
+  <p><?php the_content(__('(more...)')); ?></p>
+</div>
