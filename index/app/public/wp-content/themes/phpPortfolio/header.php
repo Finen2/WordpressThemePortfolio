ac065ee7f03f39e9465e7cdf4df1b8b15539e00b
Tim Aro <timaro@live.se>
2018-06-02 19:34:46 +0200

    save

--- index/app/public/wp-content/themes/phpPortfolio/header.php
+++ index/app/public/wp-content/themes/phpPortfolio/header.php
@@ -4,12 +4,13 @@
     <title>Tutorial theme</title>
     <script type="text/javascript" src="<?php echo get_stylesheet_directory_uri().'/js/bootstrap.js'; ?>"></script>
     <link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri().'/css/bootstrap.css'; ?>">
+    <link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri().'/css/myStyle.css'; ?>">
     <link rel="stylesheet" type="text/css" media="all" href="<?php bloginfo( 'stylesheet_url' ); ?>" />
   </head>
 
   <body>
 
-    <div id="ttr_header" class="jumbotron">
+    <div id="ttr_header" class="jumbotron sideHeader">
       <?php include('navbar.php'); ?>
     </div>
     <div class="container">
--- index/app/public/wp-content/themes/phpPortfolio/post.php
+++ index/app/public/wp-content/themes/phpPortfolio/post.php
@@ -3,3 +3,11 @@
   <h4>Posted on <?php the_time('F jS, Y') ?></h4>
   <p><?php the_content(__('(more...)')); ?></p>
 </div>
+
+<div class="col-lg-12 col-sm-12 col-md-12 col-xs-12">
+  <div class="col-lg-6 col-sm-6 col-md-6 col-xs-6">
+    <h1><?php the_title(); ?></h1>
+    <h4>Posted on <?php the_time('F jS, Y') ?></h4>
+    <p><?php the_content(__('(more...)')); ?></p>
+  </div>
+</div>
