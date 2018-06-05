63c06ae83e9f00276494979f9421c8d58442d25e
Tim Aro <timaro@live.se>
2018-06-05 13:38:57 +0200

    final commit befot delivery

--- index/app/sql/local.wp_terms.sql
+++ index/app/sql/local.wp_terms.sql
@@ -2,4 +2,6 @@
 /*!40014 SET FOREIGN_KEY_CHECKS=0*/;
 /*!40103 SET TIME_ZONE='+00:00' */;
 INSERT INTO `wp_terms` VALUES
-(1,"Uncategorized","uncategorized",0);
+(1,"Övriga","uncategorized",0),
+(2,"Navigaion","navigaion",0),
+(3,"Kristian","kristian",0);
--- index/app/sql/local.wp_usermeta.sql
+++ index/app/sql/local.wp_usermeta.sql
@@ -15,10 +15,13 @@ INSERT INTO `wp_usermeta` VALUES
 (11,1,"locale",""),
 (12,1,"wp_capabilities","a:1:{s:13:\"administrator\";b:1;}"),
 (13,1,"wp_user_level","10"),
-(14,1,"dismissed_wp_pointers",""),
+(14,1,"dismissed_wp_pointers","wp496_privacy,theme_editor_notice"),
 (15,1,"show_welcome_panel","1"),
 (16,1,"session_tokens","a:1:{s:64:\"cf54aa65313030bcfffb4e1c9dc2073ed3af2b5fec882dc428733c676ec83a3e\";a:4:{s:10:\"expiration\";i:1529065940;s:2:\"ip\";s:9:\"127.0.0.1\";s:2:\"ua\";s:121:\"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.181 Safari/537.36\";s:5:\"login\";i:1527856340;}}"),
 (17,1,"wp_dashboard_quick_press_last_post_id","7"),
 (18,1,"community-events-location","a:1:{s:2:\"ip\";s:12:\"192.168.95.0\";}"),
-(19,1,"wp_user-settings","editor=tinymce"),
-(20,1,"wp_user-settings-time","1526284930");
+(19,1,"wp_user-settings","editor=tinymce&libraryContent=browse&imgsize=full&urlbutton=file"),
+(20,1,"wp_user-settings-time","1528113600"),
+(21,1,"nav_menu_recently_edited","2"),
+(22,1,"managenav-menuscolumnshidden","a:5:{i:0;s:11:\"link-target\";i:1;s:11:\"css-classes\";i:2;s:3:\"xfn\";i:3;s:11:\"description\";i:4;s:15:\"title-attribute\";}"),
+(23,1,"metaboxhidden_nav-menus","a:1:{i:0;s:12:\"add-post_tag\";}");
